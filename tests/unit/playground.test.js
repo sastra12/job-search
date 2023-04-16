55945f79e201600927b9bae738999f8dae624dc6
sastra12 <anonymousonly594@gmail.com>
2023-04-16 21:33:40 +0700

    Multiple Describe Blocks

--- tests/unit/playground.test.js
+++ tests/unit/playground.test.js
@@ -1,7 +1,23 @@
 import { describe, it, expect } from 'vitest'
 
+import { evenOrOdd } from '@/playground.js'
+
 describe('basic math', () => {
   it('add two numbers', () => {
     expect(1 + 1).toBe(2)
+  })
+})
+
+describe('evenOrOdd', () => {
+  describe('when number is even', () => {
+    it('indicates the number is even', () => {
+      expect(evenOrOdd(4)).toBe('Even')
+    })
+  })
+
+  describe('when number is odd', () => {
+    it('indicates the number is odd', () => {
+      expect(evenOrOdd(3)).toBe('Odd')
+    })
   })
 })
