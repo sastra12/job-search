e8df7b51f0cacd94ea3be13976b9a712615b407e
sastra12 <anonymousonly594@gmail.com>
2023-04-16 23:41:10 +0700

    adding coverage flag

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <MainNav />
+  <main-nav />
 </template>
 
 <script>
-import MainNav from '@/components/MainNav.vue'
+import MainNav from "@/components/MainNav.vue";
+
 export default {
-  name: 'App',
-  components: { MainNav }
-}
+  name: "App",
+  components: {
+    MainNav,
+  },
+};
 </script>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,8 +1,13 @@
 <template>
-  <header class="w-full">
-    <div class="fixed w-full h-16">
-      <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
-        <a href="/" class="flex items-center text-xl">{{ company }}</a>
+  <header class="w-full text-sm">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+      >
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <h2 class="ml-8 flex h-full items-center">
+          Developed by {{ author.firstName }} {{ author.lastName }}
+        </h2>
       </div>
     </div>
   </header>
@@ -10,11 +15,15 @@
 
 <script>
 export default {
-  name: 'MainNav',
+  name: "MainNav",
   data() {
     return {
-      company: 'SAS Corporation'
-    }
-  }
-}
+      company: "Bobo Careers",
+      author: {
+        firstName: "Boris",
+        lastName: "Paskhaver",
+      },
+    };
+  },
+};
 </script>
