9ae526664ba04f288225d1bca272ed7f6d636abb
sastra12 <anonymousonly594@gmail.com>
2023-04-16 21:11:36 +0700

    The Basics of Vitest Syntax

---
+++ src/components/MainNav.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="w-full">
+    <div class="fixed w-full h-16">
+      <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
+        <a href="/" class="flex items-center text-xl">{{ company }}</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'SAS Corporation'
+    }
+  }
+}
+</script>
