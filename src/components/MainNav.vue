e5a6a24c1bf26eb521c721a868ebeced7f878390
sastra12 <anonymousonly594@gmail.com>
2023-04-17 20:58:15 +0700

    create action button component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -19,14 +19,20 @@
             </li>
           </ul>
         </nav>
+        <div class="ml-auto flex items-center">
+          <ActionButton />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
+
 export default {
   name: "MainNav",
+  components: { ActionButton },
   data() {
     return {
       company: "SAS Corporation",
