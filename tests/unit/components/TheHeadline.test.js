ca89343c332201dab5fe89516206e6058081c847
sastra12 <anonymousonly594@gmail.com>
2023-04-26 13:36:25 +0700

    More Tests for TheHeadline Component

--- tests/unit/components/TheHeadline.test.js
+++ tests/unit/components/TheHeadline.test.js
@@ -1,13 +1,59 @@
 import { render, screen } from "@testing-library/vue";
 
 import TheHeadline from "@/components/TheHeadline.vue";
+import { vi } from "vitest";
+import { nextTick } from "vue";
 
 describe("TheHeadline", () => {
-  describe("Vitest playground", () => {
-    it("tracks whether it has been called", () => {
-      const mockFunction = vi.fn();
-      mockFunction(1, 2);
-      expect(mockFunction).toHaveBeenCalledWith(1, 2);
+  // verifikasi kata yang dirender terlebih dahulu
+
+  // Penggunaan timer palsu atau tiruan dengan vi.useFakeTimers() pada pengujian
+  // ini bertujuan untuk mengisolasi komponen TheHeadline dan memastikan
+  // bahwa komponen tersebut hanya diuji secara spesifik tanpa
+  // terpengaruh oleh faktor eksternal seperti waktu yang tidak terkendali.
+  it("display introductory action verb", () => {
+    vi.useFakeTimers();
+    render(TheHeadline);
+
+    const actionPhrase = screen.getByRole("heading", {
+      name: /build for everyone/i,
     });
+
+    expect(actionPhrase).toBeInTheDocument();
+    vi.useRealTimers();
+
+    // Terakhir, pengujian menggunakan vi.useRealTimers() untuk
+    // mengembalikan penggunaan timer menjadi normal setelah pengujian selesai.
+    // Hal ini berguna untuk memastikan bahwa kode di lingkungan pengujian
+    // kembali ke pengaturan waktu normal dan tidak mempengaruhi pengujian
+    // selanjutnya.
+  });
+
+  it("changes action verb at a consistent interval", () => {
+    // Kemudian, fungsi vi.fn() digunakan untuk membuat mock function
+    // yang akan digunakan sebagai pengganti fungsi setInterval.
+    // Fungsi vi.stubGlobal() digunakan untuk mengganti fungsi setInterval
+    // di lingkungan global dengan mock function yang telah dibuat.
+
+    vi.useFakeTimers();
+    const mock = vi.fn();
+    vi.stubGlobal("setInterval", mock);
+    render(TheHeadline);
+    expect(mock).toHaveBeenCalled();
+    vi.useRealTimers();
+  });
+
+  it("swap action verb after interval", async () => {
+    vi.useFakeTimers();
+    render(TheHeadline);
+    vi.advanceTimersToNextTimer();
+
+    await nextTick();
+    const actionPhrase = screen.getByRole("heading", {
+      name: /create for everyone/i,
+    });
+
+    expect(actionPhrase).toBeInTheDocument();
+    vi.useRealTimers();
   });
 });
