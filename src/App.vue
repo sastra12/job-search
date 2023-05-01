6202c54fc738027988444c0b467b87fcdace74af
sastra12 <anonymousonly594@gmail.com>
2023-05-01 11:13:37 +0700

    Keep MainNav Constant, Dynamically Render Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
+  <MainNav />
   <router-view />
 </template>
 
 <script>
+import MainNav from "@/components/Navigation/MainNav.vue";
 export default {
   name: "App",
+  components: { MainNav },
 };
 </script>
