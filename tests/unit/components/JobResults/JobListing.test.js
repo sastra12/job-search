e216162885dff17e127b5ce5cc5f5e677026c428
sastra12 <anonymousonly594@gmail.com>
2023-05-05 15:58:49 +0700

    Refactoring JobListings Test

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -4,7 +4,13 @@ import { RouterLinkStub } from "@vue/test-utils";
 import JobListing from "@/components/JobResults/JobListing.vue";
 
 describe("JobListing", () => {
-  it("render job title", () => {
+  const createJobProps = (jobProps = {}) => ({
+    title: "Vue Developer",
+    organization: "PT. SAS Corp",
+    ...jobProps,
+  });
+
+  const renderJobListing = (jobProps) => {
     render(JobListing, {
       global: {
         stubs: {
@@ -13,26 +19,21 @@ describe("JobListing", () => {
       },
       props: {
         job: {
-          title: "Vue Developer",
+          ...jobProps,
         },
       },
     });
-    expect(screen.getByText("Vue Developer")).toBeInTheDocument();
+  };
+
+  it("render job title", () => {
+    const jobProps = createJobProps({ title: "Vue Progammer" });
+    renderJobListing(jobProps);
+    expect(screen.getByText("Vue Progammer")).toBeInTheDocument();
   });
 
   it("render job organization", () => {
-    render(JobListing, {
-      global: {
-        stubs: {
-          "router-link": RouterLinkStub,
-        },
-      },
-      props: {
-        job: {
-          title: "PT. SAS Corp",
-        },
-      },
-    });
-    expect(screen.getByText("PT. SAS Corp")).toBeInTheDocument();
+    const jobProps = createJobProps({ organization: "PT. ALI BABA" });
+    renderJobListing(jobProps);
+    expect(screen.getByText("PT. ALI BABA")).toBeInTheDocument();
   });
 });
