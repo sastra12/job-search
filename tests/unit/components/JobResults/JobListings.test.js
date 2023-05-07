ea634d9e7b9f4f6289da6ee8d05ead2d482700e1
sastra12 <anonymousonly594@gmail.com>
2023-05-07 21:15:18 +0700

    Fixing Tests for JobListings Component

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -1,10 +1,9 @@
 import { screen, render } from "@testing-library/vue";
 import { RouterLinkStub } from "@vue/test-utils";
-import axios from "axios";
-import JobListings from "@/components/JobResults/JobListings.vue";
-import { vi } from "vitest";
 
-vi.mock("axios");
+import JobListings from "@/components/JobResults/JobListings.vue";
+import { createTestingPinia } from "@pinia/testing";
+import { useJobsStore } from "@/stores/jobs";
 
 describe("JobListings", () => {
   const createRoute = (queryParams = {}) => ({
@@ -15,8 +14,10 @@ describe("JobListings", () => {
   });
 
   const renderjobListings = ($route) => {
+    const pinia = createTestingPinia();
     render(JobListings, {
       global: {
+        plugins: [pinia],
         mocks: {
           $route,
         },
@@ -27,17 +28,18 @@ describe("JobListings", () => {
     });
   };
   it("fetches jobs", () => {
-    axios.get.mockResolvedValue({ data: [] });
     const $route = createRoute();
     renderjobListings($route);
-    expect(axios.get).toHaveBeenCalledWith("http://myfakeapi.com/jobs");
+    const jobsStore = useJobsStore();
+    expect(jobsStore.FETCH_JOBS).toHaveBeenCalled();
   });
 
   it("display maximum of 10 jobs", async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
     const queryParams = { page: "1" };
     const $route = createRoute(queryParams);
     renderjobListings($route);
+    const jobsStore = useJobsStore();
+    jobsStore.jobs = Array(15).fill({});
     const jobListings = await screen.findAllByRole("listitem");
     expect(jobListings).toHaveLength(10);
   });
@@ -65,11 +67,13 @@ describe("JobListings", () => {
 
   describe("when user on first page", () => {
     it("it do not showing previous page", async () => {
-      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      //axios.get.mockResolvedValue({ data: Array(15).fill({}) });
       const queryParams = { page: "1" };
       const $route = createRoute(queryParams);
 
       renderjobListings($route);
+      const jobsStore = useJobsStore();
+      jobsStore.jobs = Array(15).fill({});
       await screen.findAllByRole("listitem");
       const previousLink = screen.queryByRole("link", {
         name: /previous/i,
@@ -79,11 +83,12 @@ describe("JobListings", () => {
     });
 
     it("show link to next page", async () => {
-      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
       const queryParams = { page: "1" };
       const $route = createRoute(queryParams);
 
       renderjobListings($route);
+      const jobsStore = useJobsStore();
+      jobsStore.jobs = Array(15).fill({});
       await screen.findAllByRole("listitem");
       const nextLink = screen.queryByRole("link", {
         name: /next/i,
@@ -94,10 +99,11 @@ describe("JobListings", () => {
 
   describe("when user on last page", () => {
     it("does not showing next page", async () => {
-      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
       const queryParams = { page: "2" };
       const $route = createRoute(queryParams);
       renderjobListings($route);
+      const jobsStore = useJobsStore();
+      jobsStore.jobs = Array(15).fill({});
       await screen.findAllByRole("listitem");
       const nextLink = screen.queryByRole("link", {
         name: /next/i,
@@ -105,11 +111,12 @@ describe("JobListings", () => {
       expect(nextLink).not.toBeInTheDocument();
     });
 
-    it("showi link previous page", async () => {
-      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    it("show link previous page", async () => {
       const queryParams = { page: "2" };
       const $route = createRoute(queryParams);
       renderjobListings($route);
+      const jobsStore = useJobsStore();
+      jobsStore.jobs = Array(15).fill({});
       await screen.findAllByRole("listitem");
       const previousLink = screen.queryByRole("link", {
         name: /previous/i,
