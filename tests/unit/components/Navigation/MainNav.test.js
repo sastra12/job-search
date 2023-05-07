102c65a1d2aad2b3c29e2ee04760ef5fc2eb3091
sastra12 <anonymousonly594@gmail.com>
2023-05-07 16:03:29 +0700

    Fixing our Failing MainNav Tests

--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -4,6 +4,7 @@ import { RouterLinkStub } from "@vue/test-utils";
 
 import userEvent from "@testing-library/user-event";
 import MainNav from "@/components/Navigation/MainNav.vue";
+import { createTestingPinia } from "@pinia/testing";
 
 // Fungsi render dari Vue Testing Library adalah fungsi yang digunakan untuk merender sebuah komponen Vue ke dalam virtual DOM
 // Fungsi screen.debug() pada library test Vue digunakan untuk menampilkan output atau informasi tambahan tentang komponen Vue yang sedang diuji
@@ -25,11 +26,13 @@ import MainNav from "@/components/Navigation/MainNav.vue";
 
 describe("MainNav", () => {
   const renderMainNav = () => {
+    const pinia = createTestingPinia({ stubActions: false });
     const $route = {
       name: "Home",
     };
     render(MainNav, {
       global: {
+        plugins: [pinia],
         mocks: {
           $route,
         },
