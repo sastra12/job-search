de8605e05acc541b0e8c441db87f5fb9d77dee27
sastra12 <anonymousonly594@gmail.com>
2023-05-10 10:31:43 +0700

    Styling CollapsibleAccordion Component

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
@@ -1,13 +1,26 @@
 <template>
   <div
-    class="flex w-1/5 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
+    class="flex w-3/12 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
   >
-    Job Filters Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <ActionButton text="Clear Filter" type="secondary" />
+        </div>
+      </div>
+
+      <CollapsibleAccordion />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+
 export default {
   name: "JobFiltersSidebar",
+  components: { ActionButton, CollapsibleAccordion },
 };
 </script>
