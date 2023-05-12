6cdabc61bbca16114f0d4f9a0686dc074beed41e
sastra12 <anonymousonly594@gmail.com>
2023-05-12 21:23:59 +0700

    Writing Test for Slots

--- src/components/Shared/CollapsibleAccordion.vue
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -2,6 +2,7 @@
   <div class="border-b border-solid border-brand-gray-2 py-5">
     <div
       class="flex cursor-pointer flex-wrap items-center justify-between"
+      role="button"
       @click="open"
     >
       <h3 class="text-base font-semibold">{{ header }}</h3>
