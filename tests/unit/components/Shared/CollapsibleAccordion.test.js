5ec5258c1d018d1bfe65e412a2d2c7096066db57
sastra12 <anonymousonly594@gmail.com>
2023-05-13 06:52:47 +0700

    The Usual Refactor

--- tests/unit/components/Shared/CollapsibleAccordion.test.js
+++ tests/unit/components/Shared/CollapsibleAccordion.test.js
@@ -4,7 +4,7 @@ import userEvent from "@testing-library/user-event";
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
 
 describe("CollapsibleAccrodion", () => {
-  it("render child content", async () => {
+  const renderCollapsibleAccordio = (config = {}) => {
     render(CollapsibleAccordion, {
       global: {
         stubs: {
@@ -17,29 +17,39 @@ describe("CollapsibleAccrodion", () => {
       slots: {
         default: "<h3>My nested child</h3>",
       },
+      ...config,
     });
+  };
+
+  it("render child content", async () => {
+    const props = {
+      header: "My Category",
+    };
+    const slots = {
+      default: "<h3>My nested child</h3>",
+    };
+
+    const config = { props, slots };
+
+    renderCollapsibleAccordio(config);
 
     expect(screen.queryByText("My nested child")).not.toBeInTheDocument();
     const button = screen.getByRole("button", { name: /my category/i });
     await userEvent.click(button);
     expect(screen.getByText("My nested child")).toBeInTheDocument();
   });
-});
 
-describe("when parent does not provide custom child content", () => {
-  it("renders default content", async () => {
-    render(CollapsibleAccordion, {
-      global: {
-        stubs: {
-          FontAwesomeIcon: true,
-        },
-      },
-      props: {
+  describe("when parent does not provide custom child content", () => {
+    it("renders default content", async () => {
+      const props = {
         header: "My Category",
-      },
+      };
+      const slots = {};
+      const config = { props, slots };
+      renderCollapsibleAccordio(config);
+      const button = screen.getByRole("button", { name: /my category/i });
+      await userEvent.click(button);
+      expect(screen.getByText("Whoops, somebody forget to populate me!"));
     });
-    const button = screen.getByRole("button", { name: /my category/i });
-    await userEvent.click(button);
-    expect(screen.getByText("Whoops, somebody forget to populate me!"));
   });
 });
