7bec881898bd0aa8edff39a882c8e0d9bc3c03f7
sastra12 <anonymousonly594@gmail.com>
2023-05-13 07:19:55 +0700

    Extracting Organizations Accordion to New Component

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
@@ -16,33 +16,7 @@
         <p>Place Holder for now</p>
       </collapsible-accordion>
 
-      <collapsible-accordion header="Organizations">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li class="h-8 w-1/2">
-                <input id="VueTube" type="checkbox" class="mr-3" />
-                <label for="VueTube">VueTube</label>
-              </li>
-
-              <li class="h-8 w-1/2">
-                <input id="Between Vue" type="checkbox" class="mr-3" />
-                <label for="Between Vue">Between Vue</label>
-              </li>
-
-              <li class="h-8 w-1/2">
-                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-                <label for="Et Vue Brute">Et Vue Brute</label>
-              </li>
-
-              <li class="h-8 w-1/2">
-                <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
-                <label for="Vue and a Half Men">Vue and a Half Men</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </collapsible-accordion>
+      <job-filter-sidebar-organizations />
     </section>
   </div>
 </template>
@@ -50,9 +24,13 @@
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
-
+import JobFilterSidebarOrganizations from "@/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.vue";
 export default {
   name: "JobFiltersSidebar",
-  components: { ActionButton, CollapsibleAccordion },
+  components: {
+    ActionButton,
+    CollapsibleAccordion,
+    JobFilterSidebarOrganizations,
+  },
 };
 </script>
