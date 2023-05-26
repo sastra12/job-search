bb46f4c113a81e476f53d67b93bca5a4fda7435a
sastra12 <anonymousonly594@gmail.com>
2023-05-26 20:44:00 +0700

    change JobFilterSidebarJobTypes into Composition API

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebarJobTypes.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebarJobTypes.vue
@@ -24,30 +24,45 @@
   </collapsible-accordion>
 </template>
 
-<script>
+<script setup>
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
-import { mapState, mapActions } from "pinia";
-import { useJobsStore, UNIQUE_JOB_TYPES } from "@/stores/jobs";
-import { useUserStore, ADD_SELECTED_JOB_TYPES } from "@/stores/user";
+import { useJobsStore } from "@/stores/jobs";
+import { useUserStore } from "@/stores/user";
+import { computed, ref } from "vue";
+import { useRouter } from "vue-router";
 
-export default {
-  name: "JobFilterSidebarJobTypes",
-  components: { CollapsibleAccordion },
-  data() {
-    return {
-      selectedJobTypes: [],
-    };
-  },
-  computed: {
-    ...mapState(useJobsStore, [UNIQUE_JOB_TYPES]),
-  },
+const selectedJobTypes = ref([]);
 
-  methods: {
-    ...mapActions(useUserStore, [ADD_SELECTED_JOB_TYPES]),
-    selectJobType() {
-      this.ADD_SELECTED_JOB_TYPES(this.selectedJobTypes);
-      this.$router.push({ name: "JobResults" });
-    },
-  },
+const jobStore = useJobsStore();
+const UNIQUE_JOB_TYPES = computed(() => {
+  return jobStore.UNIQUE_JOB_TYPES;
+});
+
+const router = useRouter();
+const userStore = useUserStore();
+const selectJobType = () => {
+  userStore.ADD_SELECTED_JOB_TYPES(selectedJobTypes.value);
+  router.push({ name: "JobResults" });
 };
+
+// export default {
+//   name: "JobFilterSidebarJobTypes",
+//   components: { CollapsibleAccordion },
+//   data() {
+//     return {
+//       selectedJobTypes: [],
+//     };
+//   },
+//   computed: {
+//     ...mapState(useJobsStore, [UNIQUE_JOB_TYPES]),
+//   },
+
+//   methods: {
+//     ...mapActions(useUserStore, [ADD_SELECTED_JOB_TYPES]),
+//     selectJobType() {
+//       this.ADD_SELECTED_JOB_TYPES(this.selectedJobTypes);
+//       this.$router.push({ name: "JobResults" });
+//     },
+//   },
+// };
 </script>
